3e701da2ef4314c0651cd072ddd3fc63c8668b5b
himanshu <himanshu@algodommedia.com>
2022-08-05 17:56:54 +0530

    added files

--- twitter_API/twitter_connection.py
+++ twitter_API/twitter_connection.py
@@ -4,9 +4,12 @@ import tweepy
 import logging
 
 # auth = tweepy.OAuth2BearerHandler(bearer_token=Bearer_Token)
+
 auth = tweepy.OAuth1UserHandler(API_key,API_Key_Secret,Access_Token,Access_Token_Secret)
 api = tweepy.API(auth)
 
-public_tweets = api.home_timeline()
-for tweet in public_tweets:
-    print(tweet.text)
+# client = tweepy.Client(bearer_token=Bearer_Token)
+
+# client = tweepy.Client(consumer_key=API_key,consumer_secret=API_Key_Secret,access_token=Access_Token,access_token_secret=Access_Token_Secret)
+
+
